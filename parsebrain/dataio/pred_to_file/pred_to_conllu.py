1d052c340de011bf948608cc4e0c1b5a60df0b66
pupiera <adrien.pupier.pro@gmail.com>
2022-10-14 16:52:08 +0200

    FEAT :Begining of evaluation protocol using Conll18 script.

---
+++ parsebrain/dataio/pred_to_file/pred_to_conllu.py
@@ -0,0 +1,35 @@
+ID, FORM, LEMMA, UPOS, XPOS, FEATS, HEAD, DEPREL, DEPS, MISC = range(10)  # same as eval
+label = ["ID", "FORM", "LEMMA", "UPOS", "XPOS", "FEATS", "HEAD", "DEPREL", "DEPS", "MISC"]
+
+
+def write_token_dict_conllu(data, file):
+    '''
+    Write a dict into a file in the conllu format
+
+    >>> data_ = [{"sent_id": "test1", "sentence" : [{"ID": 1, "FORM": "XXX","UPOS": "YYY", "HEAD":2, "DEPREL" :"deps"},
+    ...                                            {"ID": 2, "FORM": "XYY", "UPOS": "YXX", "HEAD":0, "DEPREL" :"roots"}]},
+    ...            {"sent_id": "test2", "sentence" : [{"ID": 1, "FORM": "XXX","UPOS": "YYY", "HEAD":2, "DEPREL" :"deps"},
+    ...                                          {"ID": 2, "FORM": "XYY", "UPOS": "YXX", "HEAD":0, "DEPREL" :"roots"}]}]
+    >>> with open("test_write.conllu","w",encoding="utf-8") as tmp_file:
+    ...     write_token_dict_conllu(data_, tmp_file)
+    '''
+    for d in data:
+        sent = ""
+        if "sent_id" in d:
+            sent = f"# sent_id = {d['sent_id']}\n"
+        sentence = d['sentence']
+        for token in sentence:
+            for l in label:
+                if l in token:
+                    sent += f"{token[l]}\t"
+                else:
+                    sent += "_\t"
+            sent = sent[0:-1]
+            sent += "\n"  # replace last \t of each line by \n
+        file.write(sent)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
